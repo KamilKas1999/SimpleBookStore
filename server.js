b31e67f5d13480ae4cd4a318e01dd611cc85213d
Kamil <kamilkasprzak1999@gmail.com>
2021-05-10 17:06:29 +0200

    heroku test

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/Bookstore'));
+app.use(express.static(__dirname + '/dist/Bookstore'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/Bookstore/'}),
-);
+app.get('/*', (req,res,next) => {
+    res.sendFile(path.join(__dirname + '/dist/Bookstore/index.html'));
+});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+app.listen(process.env.PORT || 8000);
